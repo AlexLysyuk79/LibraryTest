6eb33fdce381dafdc672a52719feb65a3ef1f68c
AlexLysyuk79 <AlexLysyuk79@gmail.com>
2022-05-30 15:55:02 -0700

    My second commit ( added journal)

---
+++ src/Journal.java
@@ -0,0 +1,16 @@
+public class Journal extends Publication {
+    private int publicationYear;
+    private int number;
+
+    Journal (String name, int countPages, int number, int publicationYear){
+        super(name, countPages);
+        this.publicationYear = publicationYear;
+        this.number = number;
+    }
+
+    @Override
+    public String print() {
+        return "Journal{" + super.print() + " , number = " + number + ", year " + publicationYear;
+    }
+}
+// Journal{name=<название_журнала>,countPages=<количество_страниц>,number=<номер_журнала>,year=<год_публикации>}
--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,12 @@ public class Main {
         System.out.println(publication.print());
 
         Book book = new Book("nazvanie",500, "AuthorBooks");
-        System.out.println("Book created >>>>>>>>>>>>>");
+        System.out.println("\nBook created >>>>>>>>>>>>>");
         System.out.println(book.print());
+
+        Journal journal = new Journal("Around the world", 345, 2, 1979);
+        System.out.println("\nJournal created >>>>>>>>>>>>>");
+        System.out.println(journal.print());
 
 
     }
